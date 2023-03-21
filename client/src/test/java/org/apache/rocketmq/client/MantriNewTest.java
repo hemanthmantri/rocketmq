d1b52a2d0e49a170814ef6db9d3a2bf60e1d5767
Hemanth Mantri <hemanth.mantri@harness.io>
2023-03-21 03:30:07 -0700

    Create MantriNewTest.java

---
+++ client/src/test/java/org/apache/rocketmq/client/MantriNewTest.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.rocketmq.client;
+
+import java.util.Properties;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.TopicConfig;
+import org.apache.rocketmq.common.constant.PermName;
+import org.apache.rocketmq.common.protocol.ResponseCode;
+import org.apache.rocketmq.common.topic.TopicValidator;
+import org.junit.Test;
+import org.junit.Assert;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Fail.failBecauseExceptionWasNotThrown;
+import static org.junit.Assert.fail;
+
+
+public class MantriNewTest {
+
+    @Test
+    public void testNewlyAddedTest1() {
+        Assert.assertEquals(1, 1);
+    }
+  
+    @Test
+    public void testNewlyAddedTest2() {
+        Assert.assertEquals(2, 2);
+    }
+  
+  
+    @Test
+    public void testNewlyAddedTest3() {
+        Assert.assertEquals(3, 3);
+    }
+}
